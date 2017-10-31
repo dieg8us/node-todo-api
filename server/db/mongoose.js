b82cc6a927eac3f579a31ceb388007b8a3a0300a
Diego Uribe Segura <diego.uribe.segura@gmail.com>
2017-10-31 13:43:45 -0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // set it up to set promises
 mongoose.Promise = global.Promise;
 // connect
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
