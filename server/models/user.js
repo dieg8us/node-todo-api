bbfbd4c658516a499d45f282d4bfe2d5a976f1a4
Diego Uribe Segura <diego.uribe.segura@gmail.com>
2017-11-08 16:40:27 -0300

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
   email: {
@@ -70,6 +71,27 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 };
+
+// Mongoose middleware
+UserSchema.pre('save', function (next) {
+  let user = this;
+
+  // validate user modified password
+  if (user.isModified('password')) {
+    // gen salt
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        // save hashed password to user
+        user.password = hash;
+        next();
+      });
+    });
+
+  } else {
+    next();
+  }
+
+});
 
 let User = mongoose.model('User', UserSchema);
 
